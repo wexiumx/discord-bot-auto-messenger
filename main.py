310e8f350130e17bea97dc69d6b6cd67d34f8225
Lukas Bludnickas <lukas.bludnickas@proton.me>
2026-09-25 22:54:06 +0300

    success, bot is working

--- main.py
+++ main.py
@@ -4,8 +4,25 @@ import random
 from dotenv import load_dotenv
 
 load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 token = os.getenv("TOKEN")
 
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == 'six':
+        await message.channel.send('seven')
 
 
+client.run(token)
 
